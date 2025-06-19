1ca5e3d979ce0f9980cb197775ac8b0aec81add2
nil-park <kwang0621@gmail.com>
2025-06-20 00:19:02 +0900

    Initialize bert-emotion-tokenizer project

---
+++ pipeline/tokenizer/bert_emotion_tokenizer/__init__.py
@@ -0,0 +1,13 @@
+from transformers import AutoTokenizer
+
+MODEL_NAME: str = "boltuix/bert-emotion"
+
+class BertEmotionTokenizer:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+    def tokenize(self, text):
+        return self.tokenizer(text, truncation=True, padding='max_length', max_length=512, return_tensors='pt')
+
+    def decode(self, token_ids):
+        return self.tokenizer.decode(token_ids, skip_special_tokens=True)
