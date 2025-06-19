405b9f95e8dc97fb042fa45608006e5e9e6da7d3
nil-park <kwang0621@gmail.com>
2025-06-20 00:42:49 +0900

    Adopt hello activity from temporalio repo

--- pipeline/tokenizer/bert_emotion_tokenizer/__init__.py
+++ pipeline/tokenizer/bert_emotion_tokenizer/__init__.py
@@ -1,13 +1,14 @@
-from transformers import AutoTokenizer
+# from transformers import AutoTokenizer
 
-MODEL_NAME: str = "boltuix/bert-emotion"
+# MODEL_NAME: str = "boltuix/bert-emotion"
 
-class BertEmotionTokenizer:
-    def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-    def tokenize(self, text):
-        return self.tokenizer(text, truncation=True, padding='max_length', max_length=512, return_tensors='pt')
+# class BertEmotionTokenizer:
+#     def __init__(self):
+#         self.tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-    def decode(self, token_ids):
-        return self.tokenizer.decode(token_ids, skip_special_tokens=True)
+#     def tokenize(self, text):
+#         return self.tokenizer(text, truncation=True, padding="max_length", max_length=512, return_tensors="pt")
+
+#     def decode(self, token_ids):
+#         return self.tokenizer.decode(token_ids, skip_special_tokens=True)
